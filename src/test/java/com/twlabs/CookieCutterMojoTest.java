b6797e5e7c5d9119db52716ff223cac23e5befb8
GiuseppeMP <giuseppemath@gmail.com>
2023-04-05 09:19:00 -0300

    #1 - chore(tests) o metodo lookupMojo precisa de uma tag configuration no projeto de testes, ao menos vazia. O lookupEmptyMojo funciona igual lookupMojo mas nao necessita da declaracao do plugin no build do projeto de testes

--- src/test/java/com/twlabs/CookieCutterMojoTest.java
+++ src/test/java/com/twlabs/CookieCutterMojoTest.java
@@ -6,7 +6,8 @@ import org.codehaus.plexus.PlexusTestCase;
 
 public class CookieCutterMojoTest extends AbstractMojoTestCase {
 
-    private static final String HELLO_WORLD_PJ = "/src/test/resources/spring-hello-world/pom.xml";
+    private static final String HELLO_WORLD_PJ = "src/test/resources/spring-hello-world/pom.xml";
+    private static final String EMPTY = "src/test/resources/no-project/pom.xml";
 
     protected void setUp() throws Exception {
         // required
@@ -21,9 +22,15 @@ public class CookieCutterMojoTest extends AbstractMojoTestCase {
 
     }
 
-    public void test_method() throws Exception {
-        CookieCutterMojo mojo = (CookieCutterMojo) this.lookupMojo("micci",
-                PlexusTestCase.getBasedir() + HELLO_WORLD_PJ);
+    public void test_mojo() throws Exception {
+        CookieCutterMojo mojo = (CookieCutterMojo) this.lookupMojo("micci", HELLO_WORLD_PJ);
+        assertNotNull(mojo);
+        mojo.execute();
+
+    }
+
+    public void test_empty_mojo() throws Exception {
+        CookieCutterMojo mojo = (CookieCutterMojo) this.lookupEmptyMojo("micci", EMPTY);
         assertNotNull(mojo);
         mojo.execute();
 
