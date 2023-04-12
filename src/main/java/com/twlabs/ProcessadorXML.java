897a41f6500f65eaf5c3946884c3469cbf9e7dfd
Micci <luiz.miccieli@gmail.com>
2023-04-12 14:40:59 -0300

    #9 - refactor(ProcessadorXML): refatorado para lançar exception para nós não encontrados

--- src/main/java/com/twlabs/ProcessadorXML.java
+++ src/main/java/com/twlabs/ProcessadorXML.java
@@ -1,7 +1,5 @@
 package com.twlabs;
 
-import java.io.File;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,7 +9,6 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
-import org.dom4j.XPathException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
@@ -21,50 +18,40 @@ import org.w3c.dom.Node;
 public class ProcessadorXML {
 
 
+
     private Document xml;
 
     public ProcessadorXML(Path path) throws Exception {
         DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = builderFactory.newDocumentBuilder();
-        System.out.println(path.toString());
-        System.out.println(path.toFile());
         xml = builder.parse(path.toFile());
 
 
     }
 
 
-    public Map<String, String> buscaParametro(String path) throws Exception {
+    public Map<String, String> buscaParametro(String path) throws XPathExpressionException {
 
         XPath xpath = XPathFactory.newInstance().newXPath();
-        Node parametro = (Node) xpath.evaluate(path, xml, XPathConstants.NODE);
-
-
         Map<String, String> map = new HashMap<String, String>();
 
-        map.put(path, parametro.getTextContent());
+        Node parametro = (Node) xpath.evaluate(path, xml, XPathConstants.NODE);
+        if (parametro == null) {
+            throw new XPathExpressionException("Prametro " + path + " não encontrado!!!");
+        }
 
+        map.put(path, parametro.getTextContent());
 
         return map;
     }
 
 
-    public Map<String, String> parse(Map<String, String> path) throws XPathExpressionException {
-
-        XPath xpath = XPathFactory.newInstance().newXPath();
+    public Map<String, String> parse(Map<String, String> paths) throws XPathExpressionException {
 
         Map<String, String> map = new HashMap<String, String>();
-        path.forEach((chave, valor) -> {
-            try {
-                Node parametroTmp = (Node) xpath.evaluate(chave, xml, XPathConstants.NODE);
-                map.put(chave, parametroTmp.getTextContent());
-
-
-            } catch (XPathExpressionException e) {
-                System.err.println("Xablau na hora de iterar o mapa de nós");
-
-            }
-        });
+        for (String path : paths.keySet()) {
+            map.put(path, buscaParametro(path).get(path));
+        }
 
         return map;
     }
