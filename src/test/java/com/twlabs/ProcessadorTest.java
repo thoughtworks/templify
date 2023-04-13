244cae6461f691b26946b3d1ad974ad8b512e2f1
Micci <luiz.miccieli@gmail.com>
2023-04-13 17:33:07 -0300

    #9 test(ProcessadorTest): add test to verify if nodes are not found

--- src/test/java/com/twlabs/ProcessadorTest.java
+++ src/test/java/com/twlabs/ProcessadorTest.java
@@ -1,6 +1,8 @@
 package com.twlabs;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.net.URL;
 import java.util.HashMap;
@@ -20,7 +22,7 @@ public class ProcessadorTest {
     String TESTREPLACED = "processador/xml/";
 
     public URL getFileFromResources(String path) {
-        return  getClass().getClassLoader().getResource(path);
+        return getClass().getClassLoader().getResource(path);
     }
 
 
@@ -40,12 +42,32 @@ public class ProcessadorTest {
         assertTrue(result.equals(actual), "Result has: \n" + result);
     }
 
+    @ParameterizedTest
+    @CsvSource({"project/notFound", "notfound/groupId"})
+    public void test_find_not_found(String query) {
+        NodeList nodes = processador.find(getFileFromResources(TESTXML).getPath(), query);
+
+        assertNull(nodes.item(0));
+
+    }
+
 
     @ParameterizedTest
-    @CsvSource({"/project/artifactId, param.artifactId"})
-    public void replace(String query, String newValue) {
-        String pathToReplaced =getFileFromResources(TESTREPLACED)+"replaced_file.xml";
-        assertDoesNotThrow(() -> processador.replace(getFileFromResources(TESTXML).getPath(), query, newValue, pathToReplaced));
+    @CsvSource({"/project/artifactId, param.artifactId",
+            "/project/groupId, Cookiecutter.test.param"})
+    public void test_replace(String query, String newValue) {
+        String pathToReplaced = getFileFromResources(TESTREPLACED) + "replaced_file.xml";
+        assertDoesNotThrow(() -> processador.replace(getFileFromResources(TESTXML).getPath(), query,
+                newValue, pathToReplaced));
+        // new File("processador/tmp/xml/teste.xml");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"/project/NotFound, param.artifactId"})
+    public void test_replace_node_not_found(String query, String newValue) {
+        String pathToReplaced = getFileFromResources(TESTREPLACED) + "replaced_file.xml";
+        assertDoesNotThrow(() -> processador.replace(getFileFromResources(TESTXML).getPath(), query,
+                newValue, pathToReplaced));
         // new File("processador/tmp/xml/teste.xml");
     }
 }
