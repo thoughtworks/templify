50919b7ce9b3f059849c0894c1fddaa92524e2ed
Micci <luiz.miccieli@gmail.com>
2023-04-14 17:41:14 -0300

    #9 test(ProcessadorTest): added test for replace using a map of parameteres

--- src/test/java/com/twlabs/ProcessadorTest.java
+++ src/test/java/com/twlabs/ProcessadorTest.java
@@ -1,9 +1,14 @@
 package com.twlabs;
 
+import static org.assertj.core.api.Assertions.entry;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.Path;
@@ -18,6 +23,7 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -76,48 +82,89 @@ public class ProcessadorTest {
 
     }
 
+    private boolean checkExpectedLenght(String query, int expectedLenght)
+            throws XPathExpressionException {
+        Document doc = readFile(Paths.get(
+                URI.create(getFileFromResources(TESTREPLACED) + "replaced_file.xml").getPath()));
+
+        XPath xpath = XPathFactory.newInstance().newXPath();
+        NodeList nodes = (NodeList) xpath.evaluate(query, doc, XPathConstants.NODESET);
+        if (nodes.getLength() == expectedLenght) {
+            return true;
+        }
+
+        throw new RuntimeException("Was expecting lenght equal " + expectedLenght + " but it was: "
+                + nodes.getLength());
+    }
+
+
 
     @ParameterizedTest
     @CsvSource({"/project/artifactId, param.artifactId",
             "/project/groupId, Cookiecutter.test.param"})
-    public void test_replace_just_one_tag(String query, String newValue) throws XPathExpressionException {
-        String pathToReplaced = getFileFromResources(TESTREPLACED)+"replaced_file.xml";
+    public void test_replace_just_one_tag(String query, String newValue)
+            throws XPathExpressionException {
+        String pathToReplaced = getFileFromResources(TESTREPLACED) + "replaced_file.xml";
         assertDoesNotThrow(() -> processador.replace(getFileFromResources(TESTXML).getPath(), query,
                 newValue, pathToReplaced));
 
-        Document doc = readFile(Paths.get(URI.create(getFileFromResources(TESTREPLACED)+"replaced_file.xml").getPath()));
-
-
-        XPath  xpath = XPathFactory.newInstance().newXPath();
-        NodeList nodes = (NodeList) xpath.evaluate(query, doc, XPathConstants.NODESET);
-        assertTrue(nodes.getLength() == 1, "Was expecting lenght equal 1 but it was: "+nodes.getLength());
-
+        assertTrue(checkExpectedLenght(query, 1), "Values mismatch");
     }
 
 
     @ParameterizedTest
     @CsvSource({"/project/dependencies/dependency/scope, Cookiecutter.scope.param"})
-    public void test_replace_more_tags(String query, String newValue) throws XPathExpressionException {
-        String pathToReplaced = getFileFromResources(TESTREPLACED)+"replaced_file.xml";
+    public void test_replace_more_tags(String query, String newValue)
+            throws XPathExpressionException {
+        String pathToReplaced = getFileFromResources(TESTREPLACED) + "replaced_file.xml";
         assertDoesNotThrow(() -> processador.replace(getFileFromResources(TESTXML).getPath(), query,
                 newValue, pathToReplaced));
 
-        Document doc = readFile(Paths.get(URI.create(getFileFromResources(TESTREPLACED)+"replaced_file.xml").getPath()));
-
-
-        XPath  xpath = XPathFactory.newInstance().newXPath();
-        NodeList nodes = (NodeList) xpath.evaluate(query, doc, XPathConstants.NODESET);
-        assertTrue(nodes.getLength() == 2, "Was expecting lenght equal 2 but it was: "+nodes.getLength());
+        assertTrue(checkExpectedLenght(query, 2), "Values mismatch");
 
     }
-
 
 
     @ParameterizedTest
     @CsvSource({"/project/NotFound, param.artifactId"})
     public void test_replace_node_not_found(String query, String newValue) {
-        String pathToReplaced = getFileFromResources(TESTREPLACED).toString();
+        String pathToReplaced = getFileFromResources(TESTREPLACED) + "replaced_file.xml";
         assertThrows(RuntimeException.class, () -> processador
                 .replace(getFileFromResources(TESTXML).getPath(), query, newValue, pathToReplaced));
     }
+
+    @Test
+    public void test_replace_with_map() {
+        Map<String, String> queryMap = new HashMap<>();
+        queryMap.put("/project/groupId", "Cookiecutter.test.replace.map.grouId");
+        queryMap.put("/project/artifactId", "Cookiecutter.test.replace.map.artifactId");
+        queryMap.put("/project/dependencies/dependency/scope", "Cookiecutter.replace.map.scopes");
+        String pathToReplaced = getFileFromResources(TESTREPLACED) + "replaced_file.xml";
+
+        assertDoesNotThrow(() -> processador.replace(getFileFromResources(TESTXML).getPath(),
+                queryMap, pathToReplaced));
+        try {
+            printFileResult(pathToReplaced);
+        } catch (IOException e) {
+            System.out.println("Didnt-read: " + getFileFromResources(TESTXML).getPath());
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    private void printFileResult(String path) throws IOException {
+        FileReader file =
+                new FileReader(getFileFromResources(TESTREPLACED + "replaced_file.xml").getPath());
+        BufferedReader reader = new BufferedReader(file);
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        reader.close();
+        file.close();
+    }
+
 }
