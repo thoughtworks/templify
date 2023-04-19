5b7fd987c7efb1fb79c765c98a54b0241eafa9fd
Micci <luiz.miccieli@gmail.com>
2023-04-19 16:20:05 -0300

    #15 - refactor(HandlerFiles): Add throws HandlerFilesException

--- src/main/java/com/twlabs/HandlerFiles.java
+++ src/main/java/com/twlabs/HandlerFiles.java
@@ -2,17 +2,18 @@
 package com.twlabs;
 
 import java.util.Map;
-import org.w3c.dom.NodeList;
 
 /**
  * Locator
  */
 public interface HandlerFiles {
 
-    Map<String, String> find(String filePath, String query);
+    Map<String, String> find(String filePath, String query) throws HandlerFilesException;
 
-    void replace(String filePath, String query, String newValue, String replacedValuesPath);
+    void replace(String filePath, String query, String newValue, String replacedValuesPath)
+            throws HandlerFilesException;
 
-    void replace(String filePath, Map<String, String> queryValueMap, String replacedValuesPath);
+    void replace(String filePath, Map<String, String> queryValueMap, String replacedValuesPath)
+            throws HandlerFilesException;
 
 }
