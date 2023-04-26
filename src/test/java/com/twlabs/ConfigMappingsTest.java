d427f30f72c096428d845e567bd89ed1c142503d
Micci <luiz.miccieli@gmail.com>
2023-04-26 13:51:06 -0300

    #19 - test(ConfigMappingsTest): Validating set Mappings

--- src/test/java/com/twlabs/ConfigMappingsTest.java
+++ src/test/java/com/twlabs/ConfigMappingsTest.java
@@ -2,8 +2,11 @@ package com.twlabs;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import com.twlabs.ConfigMappings.Mapping;
 import org.junit.jupiter.api.Test;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
@@ -15,6 +18,23 @@ public class ConfigMappingsTest {
 
         assertNotNull(actual.getMappings());
         assertEquals(0, actual.getMappings().size());
+
+    }
+
+
+    @Test
+    public void test_set_configMapping() {
+        ConfigMappings actual = new YamlMappings();
+
+
+        List<Mapping> mappingList = new ArrayList<>();
+        Mapping mapping = new Mapping("source_file", "target_file", new ArrayList<>());
+        mappingList.add(mapping);
+
+        actual.setMappings(mappingList);
+
+        assertEquals(1, actual.getMappings().size());
+        assertEquals(mapping, actual.getMappings().get(0)); 
 
     }
 
