7eb48fe247c9346b562e2a400bbd81ad4af708f9
Micci <luiz.miccieli@gmail.com>
2023-05-04 10:24:31 -0300

    #24 - chore(FileHandlerException): Added a new constructor

--- src/main/java/com/twlabs/exceptions/FileHandlerException.java
+++ src/main/java/com/twlabs/exceptions/FileHandlerException.java
@@ -1,9 +1,16 @@
 package com.twlabs.exceptions;
 
+import java.io.IOException;
+
 public class FileHandlerException extends Exception {
 
     public FileHandlerException(String errorMessage) {
         super(errorMessage);
+    }
+
+    public FileHandlerException(String errorMessage, Exception e) {
+        super(errorMessage, e);
+
     }
 }
 
