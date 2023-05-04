7f9423a978ed8699fcf25fcb05851cef604aa4df
Micci <luiz.miccieli@gmail.com>
2023-05-04 10:21:55 -0300

    #24 - test(YamlHandlerTest): Added find test

--- src/test/java/com/twlabs/handlers/YamlHandlerTest.java
+++ src/test/java/com/twlabs/handlers/YamlHandlerTest.java
@@ -1,5 +1,7 @@
 package com.twlabs.handlers;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,10 +20,38 @@ public class YamlHandlerTest {
     final String test_yaml = "src/test/resources/processador/yaml/test.yml";
 
     @ParameterizedTest
-    @CsvSource({"metadata.name"})
-    public void test_find_simple_names_must_return_values(String query)
+    @CsvSource({"metadata.name, example", "kind, Deployment",
+            "placeholders[0].query, /project/groupId",
+            "placeholders[0].name, Cookiecutter.param.groupId",
+            "placeholders[2].query, /project/dependencies/dependency/scope[text()='test']",
+            "placeholders[2].name, Cookiecutter.replace.map.scopes"})
+    public void test_find_simple_names_must_return_values(String query, String expected)
             throws FileHandlerException {
-        assertThat(this.yamlHandler.find(test_yaml, query)).isNotNull().isNotEmpty();
+        assertThat(this.yamlHandler.find(test_yaml, query).get(query)).isNotNull().isNotEmpty()
+                .isEqualTo(expected);
     }
 
+
+
+    @ParameterizedTest
+    @CsvSource({"src/test/resources/processador/yaml/empty.yml"})
+    public void test_file_is_empty(String filePath) throws FileHandlerException {
+        assertThrows(FileHandlerException.class, () -> this.yamlHandler.find(filePath, "name"));
+    }
+
+
+
+    @ParameterizedTest
+    @CsvSource({"metadata.notFound", "NotFound"})
+    public void test_find_not_found(String query) throws FileHandlerException {
+        assertThrows(FileHandlerException.class, () -> this.yamlHandler.find(test_yaml, query));
+    }
+
+
+
+    @ParameterizedTest
+    @CsvSource({"placeholders.query"})
+    public void test_find_same_path_different_values(String query) throws FileHandlerException {
+        assertThrows(FileHandlerException.class, () -> this.yamlHandler.find(test_yaml, query));
+    }
 }
