5f529440384a171c66ce830b358cc73cf7d6a263
Micci <luiz.miccieli@gmail.com>
2023-05-04 12:36:12 -0300

    #24 - feat(YamlHandler): Implemented replace method

--- src/main/java/com/twlabs/handlers/YamlHandler.java
+++ src/main/java/com/twlabs/handlers/YamlHandler.java
@@ -2,6 +2,8 @@ package com.twlabs.handlers;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,7 +35,8 @@ public class YamlHandler implements FileHandler {
                 throw new FileHandlerException("No data found in file: " + filePath);
             }
         } catch (IOException | IllegalStateException e) {
-            throw new FileHandlerException("Problem to read file: "+filePath+"\nor multiples data was found for the query: "+query, e);
+            throw new FileHandlerException("Problem to read file: " + filePath
+                    + "\nor multiples data was found for the query: " + query, e);
         }
 
         return yamlMap;
@@ -44,13 +47,33 @@ public class YamlHandler implements FileHandler {
     @Override
     public void replace(String filePath, String query, String newValue)
             throws FileHandlerException {
-        throw new UnsupportedOperationException("Unimplemented method 'replace'");
+
+        try {
+            String result = YamlPath.from(new FileInputStream(new File(filePath)))
+                    .write(query, newValue).dumpAsString();
+
+            try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
+                byte[] strToBytes = result.getBytes();
+                outputStream.write(strToBytes);
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new FileHandlerException("File not found: " + filePath, e);
+        } catch (IOException e) {
+            throw new FileHandlerException("Problem to read file: " + filePath, e);
+        }
+
     }
 
     @Override
     public void replace(String filePath, Map<String, String> queryValueMap)
             throws FileHandlerException {
-        throw new UnsupportedOperationException("Unimplemented method 'replace'");
+ 
+
+        for (Map.Entry<String, String> entry : queryValueMap.entrySet()) {
+            this.replace(filePath, entry.getKey(), entry.getValue());
+        }
+
     }
 
 }
