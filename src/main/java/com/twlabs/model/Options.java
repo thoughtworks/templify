85e3b4faad6a0519db88046be90e3b9bb62d5755
Peppe <giuseppemath@gmail.com>
2023-07-09 16:33:58 -0300

    feat(settings): add extensions attr to options model and tests

--- src/main/java/com/twlabs/model/Options.java
+++ src/main/java/com/twlabs/model/Options.java
@@ -1,5 +1,7 @@
 package com.twlabs.model;
 
+import java.util.Map;
+
 /**
  * Options
  */
@@ -8,9 +10,17 @@ public class Options {
     String placeholderPrefix;
     String placeholderSuffix;
 
+    Map<String, String> extensions;
+
     public Options(String prefix, String suffix) {
         this.placeholderPrefix = prefix;
         this.placeholderSuffix = suffix;
+    }
+
+    public Options(String prefix, String suffix, Map<String, String> extensions) {
+        this.placeholderPrefix = prefix;
+        this.placeholderSuffix = suffix;
+        this.extensions = extensions;
     }
 
     public String getPlaceholderPrefix() {
@@ -19,6 +29,10 @@ public class Options {
 
     public String getPlaceholderSuffix() {
         return this.placeholderSuffix;
+    }
+
+    public Map<String, String> getExtensions() {
+        return extensions;
     }
 
 }
--- src/test/java/com/twlabs/model/OptionsTest.java
+++ src/test/java/com/twlabs/model/OptionsTest.java
@@ -1,6 +1,7 @@
 package com.twlabs.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -14,9 +15,10 @@ public class OptionsTest {
 
         String prefix = "_{{";
         String suffix = "}}_";
+        Map<String, String> extensions = Map.of("k1",  "v1",  "k2", "v2");
 
-        assertThat(new Options(prefix, suffix)).isNotNull()
-                .extracting(Options::getPlaceholderPrefix, Options::getPlaceholderSuffix)
-                .containsExactly(prefix, suffix);
+        assertThat(new Options(prefix, suffix, extensions)).isNotNull()
+                .extracting(Options::getPlaceholderPrefix, Options::getPlaceholderSuffix, Options::getExtensions)
+                .containsExactly(prefix, suffix, extensions);
     }
 }
