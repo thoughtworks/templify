2cfdac76da4d3bbcc066aedeb6f5ebccd477d0bb
Peppe <giuseppemath@gmail.com>
2023-08-19 19:22:38 -0300

    test(ExecuteStepsTaskTest) should execute fileHandler kind executor

---
+++ src/test/java/com/twlabs/services/tasks/ExecuteStepsTaskTest.java
@@ -0,0 +1,79 @@
+package com.twlabs.services.tasks;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.io.FilenameUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.Mockito;
+import com.twlabs.kinds.FileHandlerKind;
+import com.twlabs.kinds.KindExecutor;
+import com.twlabs.model.settings.PluginConfig;
+import com.twlabs.model.settings.StepsKindTemplate;
+import com.twlabs.services.CreateTemplateRequest;
+import com.twlabs.services.CreateTemplateRequest.CreateTemplateRequestBuilder;
+
+/**
+ * ExecuteStepsTaskTest
+ */
+public class ExecuteStepsTaskTest {
+
+
+    private static final String BUILD_TEMPLATE_DIR = "/template";
+
+    @Test
+    public void test_execute_steps(@TempDir Path tmpDir) {
+
+        ExecuteStepsTask executeStepsTask = new ExecuteStepsTask();
+
+        KindExecutor fileHandlerKind = mock(FileHandlerKind.class);
+
+        executeStepsTask.setFileHandlerKind(fileHandlerKind);
+
+        String baseDir =
+                "src/test/resources-its/com/twlabs/CookieCutterMojoIT/configuracao_basica_build_test/";
+
+        CreateTemplateRequestBuilder requestBuilder = new CreateTemplateRequestBuilder();
+
+        String buildDir = baseDir + "target/";
+
+        String tempTemplateDir = tmpDir.toFile().getAbsolutePath() + BUILD_TEMPLATE_DIR;
+
+        requestBuilder
+                .withBaseDir(new File(baseDir))
+                .withBuildDir(buildDir)
+                .withConfiguration(this.createConfigTest())
+                .withTemplateDir(tempTemplateDir);
+
+        CreateTemplateRequest execute = executeStepsTask.execute(requestBuilder.build());
+
+        verify(fileHandlerKind, Mockito.times(1)).execute(Mockito.any(StepsKindTemplate.class),
+                Mockito.any(CreateTemplateRequest.class));
+
+        assertNotNull(execute);
+
+    }
+
+    public PluginConfig createConfigTest() {
+
+
+        StepsKindTemplate stepsKindTemplate = new StepsKindTemplate();
+        stepsKindTemplate.setKind("FileHandler");
+
+        List<StepsKindTemplate> steps = new ArrayList<>();
+        steps.add(stepsKindTemplate);
+
+        PluginConfig configuration = new PluginConfig();
+        configuration.setSteps(steps);
+
+        return configuration;
+    }
+
+
+}
