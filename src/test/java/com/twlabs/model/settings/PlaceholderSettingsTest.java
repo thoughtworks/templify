38750c215f553da58f815c1e5d3ff4855d37f8c3
Luiz-Micci <luiz.miccieli@thoughtworks.com>
2023-10-06 22:01:59 -0300

    #37 - test(PlaceholderSettings): Resolve mutation conditional at isEmpty method

---
+++ src/test/java/com/twlabs/model/settings/PlaceholderSettingsTest.java
@@ -0,0 +1,27 @@
+package com.twlabs.model.settings;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ * PlaceholderSettingsTest
+ */
+public class PlaceholderSettingsTest {
+
+
+    @Test
+    public void test_PlaceholderSettings_is_empty() throws Exception {
+
+        PlaceholderSettings settings = new PlaceholderSettings();
+        assertTrue(settings.isEmpty());
+    }
+
+
+    @Test
+    public void test_PlaceholderSettings_is_not_empty() throws Exception {
+
+        PlaceholderSettings settings = new PlaceholderSettings("foo", "bar");
+        assertFalse(settings.isEmpty());
+    }
+}
