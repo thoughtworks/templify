ce93b579589912c464676f6b1088b2d4a9fce00e
Peppe <giuseppemath@gmail.com>
2023-12-12 13:40:04 -0300

    (feat): script make tdd now run in watch mode

--- scripts/tdd-modified-files.sh
+++ scripts/tdd-modified-files.sh
@@ -1,23 +1,32 @@
-# for s in `git diff --name-only HEAD~`
-for s in `git diff --name-only`
+#!/bin/sh
+
+test(){
+    # for s in `git diff --name-only HEAD~`
+    for s in `git diff --name-only`
+    do
+        classes=""
+        if [[ $s == *".java" ]]; then
+            s=$(echo $s | tr '/' '.')
+            s=$(echo ${s%.java})
+            s=${s/.java/}
+        if [[ $s != *"Test.java" ]]; then
+            s=${s/src./}Test
+        fi
+            s=$(echo "$s" | sed 's/.*\.//')
+            classes=$classes,$s
+        fi
+    done
+    if [ -n "$classes" ]; then
+        echo " -> Test Suite: ${classes:1}"
+        mvn test -Dtest="${classes:1}" -DunitOnly=true
+    else
+        echo " -> No modified java files found"
+    fi
+}
+
+KEY='y'
+while [ $KEY == 'y' ]
 do
-if [[ $s == *".java" ]]; then
-  s=$(echo $s | tr '/' '.')
-  s=$(echo ${s%.java})
-  s=${s/.java/}
-  if [[ $s != *"Test.java" ]]; then
-    s=${s/src./}Test
-  fi
-  s=$(echo "$s" | sed 's/.*\.//')
-  classes=$classes,$s
-fi
+    test
+    read -n1 -p  "You are in watch mode, re-run tests? [y/n]" -s KEY
 done
-## if classes is not empty
-
-if [ -n "$classes" ]; then
-  echo " -> Test Suite: ${classes:1}"
-  mvn test -Dtest="${classes:1}" -DunitOnly=true
-else
-  echo " -> No modified java files found"
-fi
-
