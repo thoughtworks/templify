44e4a8801045514868ecffeacfafdf6f25867007
GiuseppeMP <giuseppemath@gmail.com>
2024-12-05 16:32:31 +0100

    (fix): kill some mutations.

---
+++ src/test/java/com/twlabs/kinds/handlers/base/KindPlaceholderTest.java
@@ -0,0 +1,15 @@
+package com.twlabs.kinds.handlers.base;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class KindPlaceholderTest {
+
+    @Test
+    public void test() {
+        KindPlaceholder kindPlaceholder = new KindPlaceholder("match", "replace");
+        assertEquals("match", kindPlaceholder.getMatch());
+        assertEquals("replace", kindPlaceholder.getReplace());
+    }
+}
+
